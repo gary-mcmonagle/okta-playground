2a97ba5b033d92db43a86a35e53e6758b23e3745
gary-mcmonagle <gary.mcmonagle@15gifts.com>
2023-04-13 21:12:09 +0100

    Working

--- nativeapp/myapp/App.tsx
+++ nativeapp/myapp/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -8,8 +8,16 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import axios from 'axios';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {
+  createConfig,
+  signIn,
+  signOut,
+  getAccessToken,
+  signInWithBrowser,
+} from '@okta/okta-react-native';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -17,6 +25,32 @@ function App(): JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => {
+    (async () => {
+      await createConfig({
+        issuer: 'https://dev-56089123.okta.com/oauth2/aus934m1fmAYT5lN95d7', // Optional
+        clientId: '0oa93yqn1uqQ9aLCn5d7',
+        redirectUri: 'com.okta.dev-56089123:/callback',
+        endSessionRedirectUri: 'com.okta.dev-56089123:/',
+        discoveryUri:
+          'https://dev-56089123.okta.com/oauth2/aus934m1fmAYT5lN95d7',
+        scopes: ['openid', 'profile', 'offline_access'],
+        requireHardwareBackedKeyStore: true,
+      });
+      const signIn = await signInWithBrowser();
+      console.log({signIn});
+      const resp = await axios.get(
+        'https://oktatestfnapp.azurewebsites.net/api/garytest',
+        {
+          headers: {
+            Authorization: `Bearer ${signIn.access_token}`,
+          },
+        },
+      );
+      console.log({resp});
+    })();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
